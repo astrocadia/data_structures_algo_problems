50664068712f65e0d7842bad67c892520b209e20
astrocadia <pflueger.mike@gmail.com>
2025-04-07 15:43:56 -0400

    problem 1_1

---
+++ 1_arrays_strings/1_1_is_unique.py
@@ -0,0 +1,36 @@
+
+test_strings = ['abcdefg', 'abcdefgg', 'aabcdefg', 'a bc']
+
+def is_unique(test_string: str) -> bool:
+	compare_set = set()
+	for char in test_string:
+		if char in compare_set:
+			return False
+		compare_set.add(char)
+	return True
+
+def is_unique_bitwise(test_string: str) -> bool:
+	checker = 0
+	for char in test_string:
+		val = ord(char)
+		if checker & (1 << val) > 0:
+			return False
+		checker |= (1 << val)
+	return True
+
+def is_unique_bool(test_string: str) -> bool:
+	if len(test_string) > 128:
+		return False
+	char_list = [False]*128
+	for char in test_string:
+		if char_list[ord(char)]:
+			return False
+		else:
+			char_list[ord(char)] = True
+	return True
+
+for string in test_strings:
+	print(is_unique(string))
+	print(is_unique_bitwise(string))
+	print(is_unique_bool(string))
+	print('')
